feb6456074aa6ae2c7d6226187eea74e98c9f69a
Michael Mills <michaelmills@quickenloans.com>
2018-04-13 09:38:52 -0400

    Initial commit

---
+++ Website/Controllers/AvengerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class AvengerController : Controller
+    {
+        // GET: Avenger
+        public ActionResult Index()
+        {
+            var heroList = new List<Avenger>
+            {
+                new Avenger() { Name = "Iron Man", Identity = "Tony Stark", Weapon = "Repulsor Cannon"},
+                new Avenger() { Name = "Captain America", Identity = "Steve Rogers", Weapon = "Shield"},
+                new Avenger() { Name = "Black Panther", Identity = "T'Challa", Weapon = "Vibranium Suit"},
+                new Avenger() { Name = "Thor", Identity = "Thor Odinson", Weapon = "Mjolnir"},
+                new Avenger() { Name = "Hulk", Identity = "Bruce Banner", Weapon = "Anger"},
+                new Avenger() { Name = "Spider-Man", Identity = "Peter Parker", Weapon = "Webs"},
+            };
+            return View(heroList);
+        }
+    }
+}
---
+++ Website/Models/Avenger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class Avenger
+    {
+        public string Name { get; set; }
+        public string Identity { get; set; }
+        public string Weapon { get; set; }
+
+    }
+}
